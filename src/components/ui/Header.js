5ed4a7ad56e756b33d868a7f08003091e33f0d62
loretta-arineva <wowshifter@gmail.com>
2021-07-21 17:38:28 +0100

    Set correct active link on page reload

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Material UI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,6 +8,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
     // Puts a cushion behind our nav bar so that the next content is visible 
@@ -58,6 +59,24 @@ const Header = () => {
         setValue(newValue);
     };
 
+    // Correct page refresh default active link always being home 
+
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0) {
+            setValue(0);
+        } else if (window.location.pathname === "/services" && value !== 1) {
+            setValue(1);
+        } else if (window.location.pathname === "/revolution" && value !== 2) {
+            setValue(2);
+        } else if (window.location.pathname === "/about" && value !== 3) {
+            setValue(3);
+        } else if (window.location.pathname === "/contact" && value !== 4) {
+            setValue(4);
+        } else if (window.location.pathname === "/estimate" && value !== 5) {
+            setValue(5);
+        }
+    }, [])
+
     return (
         <>
             <ElevationScroll>
@@ -65,11 +84,11 @@ const Header = () => {
                     <Toolbar disableGutters>
                         <img src={logo} alt="Arc Development Company Logo" className={classes.logo} />
                         <Tabs value={value} onChange={handleChange} aria-label="Main Navigation" className={classes.tabContainer} indicatorColor="primary">
-                            <Tab className={classes.tab} label="Home" />
-                            <Tab className={classes.tab} label="Services" />
-                            <Tab className={classes.tab} label="The Revolution" />
-                            <Tab className={classes.tab} label="About Us" />
-                            <Tab className={classes.tab} label="Contact Us" />
+                            <Tab className={classes.tab} label="Home" component={Link} to="/" />
+                            <Tab className={classes.tab} label="Services" component={Link} to="/services" />
+                            <Tab className={classes.tab} label="The Revolution" component={Link} to="revolution" />
+                            <Tab className={classes.tab} label="About Us" component={Link} to="about" />
+                            <Tab className={classes.tab} label="Contact Us" component={Link} to="contact" />
                         </Tabs>
                         <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
                     </Toolbar>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
