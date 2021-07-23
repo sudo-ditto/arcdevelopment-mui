6d49a42cb4aebadc35b355d6da7240b538614813
loretta-arineva <wowshifter@gmail.com>
2021-07-23 20:34:08 +0100

    React router setup for gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
