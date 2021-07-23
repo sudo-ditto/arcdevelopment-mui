55018aec85bbb736928bccff926aa560c9ea6793
loretta-arineva <wowshifter@gmail.com>
2021-07-23 19:13:20 +0100

    Refactored active links

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -116,11 +116,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const menuOptions = [
-    { name: 'Services', link: '/services', },
-    { name: 'Custom Software Development', link: '/customSoftware', },
-    { name: 'Mobile App Development', link: '/mobileapps', },
-    { name: 'Website Development', link: '/websites', },
-]
+    { name: 'Services', link: '/services', activeIndex: 1, selectedIndex: 0 },
+    { name: 'Custom Software Development', link: '/customSoftware', activeIndex: 1, selectedIndex: 1 },
+    { name: 'Mobile App Development', link: '/mobileapps', activeIndex: 1, selectedIndex: 2 },
+    { name: 'Website Development', link: '/websites', activeIndex: 1, selectedIndex: 3 },
+];
+
+const routes = [{ name: "Home", link: "/", activeIndex: 0 }, { name: "Services", link: "/services", activeIndex: 1 }, { name: "The Revolution", link: "/revolution", activeIndex: 2 }, { name: "About Us", link: "/about", activeIndex: 3 }, { name: "Contact Us", link: "/contact", activeIndex: 4 }];
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -174,80 +176,22 @@ const Header = () => {
     // Correct page refresh default active link always being home 
 
     useEffect(() => {
-        switch (window.location.pathname) {
-            case "/": {
-                if (value !== 0) {
-                    setValue(0);
-                }
-                break;
+        [...menuOptions, ...routes].forEach(route => {
+            switch (window.location.pathname) {
+                case `${route.link}`:
+                    if (value !== route.activeIndex) {
+                        setValue(route.activeIndex);
+                        if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+                            setSelectedIndex(route.selectedIndex);
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
-
-            case "/services": {
-                if (value !== 1) {
-                    setValue(1);
-                    setSelectedIndex(0);
-                }
-                break;
-            }
-
-            case "/customSoftware": {
-                if (value !== 1) {
-                    setValue(1);
-                    setSelectedIndex(1);
-                }
-                break;
-            }
-
-            case "/mobileapps": {
-                if (value !== 1) {
-                    setValue(1);
-                    setSelectedIndex(2);
-                }
-                break;
-            }
-
-            case "/websites": {
-                if (value !== 1) {
-                    setValue(1);
-                    setSelectedIndex(3);
-                }
-                break;
-            }
-
-            case "/revolution": {
-                if (value !== 2) {
-                    setValue(2);
-                }
-                break;
-            }
-
-            case "/about": {
-                if (value !== 3) {
-                    setValue(3);
-                }
-                break;
-            }
-
-            case "/contact": {
-                if (value !== 4) {
-                    setValue(4);
-                }
-                break;
-            }
-
-            case "/estiamte": {
-                if (value !== 5) {
-                    setValue(5);
-                }
-                break;
-            }
-
-            default:
-                break;
-        }
-
+        });
         // eslint-disable-next-line 
-    }, [])
+    }, [value, menuOptions, selectedIndex, routes])
 
     const tabs = (
         <>
