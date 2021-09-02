fcc3987b378ea7b8854020c75ecf59e98d73bf6a
loretta-arineva <loretta.arineva@gmail.com>
2021-09-02 15:29:03 +0200

    Imported estimate assets

---
+++ src/pages/Estimate.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import check from '../assets/check.svg';
+import send from '../assets/send.svg';
+import software from '../assets/software.svg';
+import mobile from '../assets/mobile.svg';
+import backArrow from '../assets/backArrow.svg';
+import forwardArrow from '../assets/forwardArrow.svg';
+import backArrowDisabled from '../assets/backArrowDisabled.svg';
+import forwardArrowDisabled from '../assets/forwardArrowDisabled.svg';
+import camera from '../assets/camera.svg';
+import upload from '../assets/upload.svg';
+import person from '../assets/person.svg';
+import persons from '../assets/persons.svg';
+import people from '../assets/people.svg';
+import info from '../assets/info.svg';
+import bell from '../assets/bell.svg';
+import users from '../assets/users.svg';
+import iphone from '../assets/iphone.svg';
+import gps from '../assets/gps.svg';
+import customized from '../assets/customized.svg';
+import data from '../assets/data.svg';
+import android from '../assets/android.svg';
+import globe from '../assets/globe.svg';
+import biometrics from '../assets/biometrics.svg';
+
+import estimateAnimation from '../animations/estimateAnimation/data.json';
+
+const Estimate = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Estimate;
